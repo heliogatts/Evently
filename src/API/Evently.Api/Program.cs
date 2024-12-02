dd9c5186a5f1efadd94a41011c2739f59e4c579b
Hélio Gatts Junior <heliogatts@gmail.com>
2024-12-02 17:21:21 -0300

    event module

--- src/API/Evently.Api/Program.cs
+++ src/API/Evently.Api/Program.cs
@@ -1,7 +1,12 @@
+using Evently.Api.Extensions;
+using Evently.Modules.Events.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddEventsModule(builder.Configuration);
 
 var app = builder.Build();
 
@@ -9,7 +14,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
+
+EventsModule.MapEndpoints(app);
+
 app.Run();
